3120fc484ee90b1e03fe4f7c7be535414a53bd28
Jake <jacob.michael.mayer@gmail.com>
2019-02-05 19:14:52 -0500

    object is found if all coordinates set

--- lib/letter.rb
+++ lib/letter.rb
@@ -5,11 +5,15 @@
 #
 class Letter
   attr_reader :char
-  attr_accessor :x, :y
+  attr_writer :x, :y
 
   def initialize(char, x_corr = nil, y_corr = nil)
     @char = char.upcase
     @x = x_corr
     @y = y_corr
+  end
+
+  def found?
+    @x && @y
   end
 end
--- spec/letter_spec.rb
+++ spec/letter_spec.rb
@@ -3,19 +3,28 @@
 require 'letter'
 
 RSpec.describe Letter.new('c') do
-  it { is_expected.to have_attributes char: 'C', x: nil, y: nil }
+  it { is_expected.to have_attributes char: 'C' }
 
-  it '#x= sets x corrdinates' do
-    described_class.x = 1
-    expect(described_class.x).to eq 1
+  it '#found? is false if all coordinates are not set' do
+    expect(described_class.found?).to be_falsey
   end
 
-  it '#y= sets y corrdinates' do
+  it '#found? is false if one coordinate is not set' do
+    described_class.x = 2
+    expect(described_class.found?).to be_falsey
+  end
+
+  it '#found? is true if both coordinates are set' do
     described_class.y = 1
-    expect(described_class.y).to eq 1
+    described_class.x = 2
+    expect(described_class.found?).to be_truthy
   end
 
   context Letter.new('C', 1, 2) do
-    it { is_expected.to have_attributes char: 'C', x: 1, y: 2 }
+    it { is_expected.to have_attributes char: 'C' }
+
+    it '#found? is true if corrdinates are set' do
+      expect(described_class.found?).to be_truthy
+    end
   end
 end
