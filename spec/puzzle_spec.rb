e93b4adf052925567f1458cc12f5ab9375a3a0bd
Jake <jacob.michael.mayer@gmail.com>
2019-02-05 22:50:56 -0500

    Build puzzle object

---
+++ spec/puzzle_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'puzzle'
+
+RSpec.describe Puzzle.new do
+  it '#parse converts formatted string to puzzle' do
+    described_class.parse("C,B\nR,T")
+    expected_letter = described_class[0][0]
+    expect(expected_letter).to be_a Letter
+    expect(expected_letter.char).to be_a 'C'
+    expect(expected_letter.found?).to be_truthy
+  end
+end
