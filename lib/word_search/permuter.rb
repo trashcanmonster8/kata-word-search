9aba1f18e20a6dd806f5108e66df749a4ca17d01
Jake <jacob.michael.mayer@gmail.com>
2019-02-09 07:38:13 -0500

    fixed typo in instance variable name

--- lib/word_search/permuter.rb
+++ lib/word_search/permuter.rb
@@ -8,7 +8,7 @@ module WordSearch
 
         def initialize(line, modulus)
             @line = line
-            @moduleus = modulus
+            @modulus = modulus
             @permutations = []
         end
     end
