00362aeefbb8bff5670e5075b1c497b159704529
Jake <jacob.michael.mayer@gmail.com>
2019-03-09 12:39:53 -0500

    create solution class

---
+++ lib/word_search/solver.rb
@@ -0,0 +1,9 @@
+module WordSearch
+  class Solver
+    attr_reader :board, :word_bank, :solution
+
+    def initialize
+      @solution = []
+    end
+  end
+end
---
+++ spec/solver_spec.rb
@@ -0,0 +1,7 @@
+require 'word_search/solver'
+
+RSpec.describe WordSearch::Solver do
+  subject { WordSearch::Solver.new }
+
+  it { is_expected.to have_attributes(board: nil, word_bank: nil, solution: []) }
+end
