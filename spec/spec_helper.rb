2f5fbdead5d117c7c03af58acd5b4f71b20de882
Jake <jacob.michael.mayer@gmail.com>
2019-03-10 15:48:16 -0400

    Added stricter configuration for mocks

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,11 @@ require './spec/helpers/puzzle_helpers'
 
 Bundler.setup
 
-RSpec.configure do |c|
-  c.include PuzzleHelpers
-  c.extend PuzzleHelpers
+RSpec.configure do |config|
+  config.include PuzzleHelpers
+  config.extend PuzzleHelpers
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_doubled_constant_names = true
+  end
 end
