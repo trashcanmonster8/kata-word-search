d723602e0609c34749ab1277fcce4bc86bbb829d
Jake <jacob.michael.mayer@gmail.com>
2019-03-10 00:48:29 -0500

    collect all arrangements before solving

--- lib/word_search/solver.rb
+++ lib/word_search/solver.rb
@@ -1,15 +1,27 @@
 # frozen_string_literal: true
 
+require 'word_search/puzzle/board'
+
 module WordSearch
   #
   # Implements common and puzzle objects to solve
   # a word search puzzle
   #
   class Solver
-    attr_reader :board, :word_bank, :solution
+    attr_reader :solution
+    attr_writer :board, :word_bank
 
     def initialize
       @solution = []
+    end
+
+    def collect_arrangements
+      @arrangements = Puzzle::Board::ARRANGEMENTS.collect do |arrangement|
+        @board.send(arrangement)
+      end
+      @arrangements << Puzzle::Board::ARRANGEMENTS.collect do |arrangement|
+        @board.send(arrangement, :reverse)
+      end
     end
   end
 end
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -5,9 +5,14 @@ require 'word_search/solver'
 RSpec.describe WordSearch::Solver do
   subject { WordSearch::Solver.new }
 
-  it {
-    is_expected.to have_attributes(board: nil,
-                                   word_bank: nil,
-                                   solution: [])
-  }
+  it { is_expected.to have_attributes(solution: []) }
+
+  it '#collect_arrangements' do
+    subject.board = board = instance_double(WordSearch::Puzzle::Board)
+    WordSearch::Puzzle::Board::ARRANGEMENTS.each do |arrangement|
+      expect(board).to receive(arrangement).with(no_args)
+      expect(board).to receive(arrangement).with(:reverse)
+    end
+    subject.collect_arrangements
+  end
 end
